4e8390f9c6fb605f5fb2b81560f02a9e27be4ea4
chatmongcol <cdadwngs@gmail.com>
2022-12-06 16:37:03 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react"
+import { Route, Routes } from "react-router-dom"
 import RootLayout from "./components/layouts/RootLayout"
-import Counter from "./components/pages/Counter"
+import About from "./components/pages/About"
+import Home from "./components/pages/Home"
+import Profile from "./components/pages/Profile"
 
 function App() {
-  console.log("process.env.PORT: ", process.env.PORT) 
+  console.log("process.env.PORT: ", process.env.PORT)
   return (
     <>
       <RootLayout>
-        <div>
-          <Counter />
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <Home />
       </RootLayout>
     </>
   )
